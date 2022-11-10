e49380e126f85bd52e338a021ac22f2d904c7f94
thuluu03 <98506834+thuluu03@users.noreply.github.com>
2022-11-10 18:53:17 -0500

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 
+import BakeryItem from "./components/BakeryItem.js"
+
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
@@ -11,21 +13,36 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleClick = (item, price) => {
+    setCart([...cart, item])
+    setCount(count + price)
+}
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Thu's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components iterates through the data and does something
+        // <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem image = {item.image} name = {item.name} description = {item.description} price = {item.price} handleClick = {handleClick}/>
       ))}
+
+      {/* {items.map((item) => (
+        <div>item: {cart}</div>
+      ))} */}
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        <h2>Total Price: {count}</h2>
+        <h4>Items: {cart}</h4>
       </div>
     </div>
   );
 }
-
+/* 
+*/
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,25 @@
 // TODO: create a component that displays a single bakery item
+
+//props is one big object that we can pass in
+//instead of just writing props, can just pass in {name, price}
+export default function BakeryItem(props) {
+    //want to pass in bakery item names and other details, use props
+
+    //this is an extra step, can just copy the commands "props.name" into the curly braces 
+    const name = props.name;
+    const price = props.price;
+    const description = props.description;
+    const image = props.image;
+    const handleClick = props.handleClick;
+    //method 1
+
+    return (
+        <div>
+            <img src={image} />
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <p>{price}</p>
+            <button onClick={() => handleClick(name, price)}>Add to Cart</button>
+        </div>
+    )
+}
